bd98fcda3f508dcbd5f3e367ed38ecc6d342f374
Adam Kimball <adamchasekimball@gmail.com>
2018-02-10 13:33:19 -0700

    response and request set up

--- server.js
+++ server.js
@@ -1,16 +1,53 @@
 var http = require('http')
+var home = require('./routes/home');
 
 var server = http.createServer();
 var port = '8080';
 var address = 'http://localhost' + port;
 
+function router(method, url, res) {
+  switch(url) {
+    case '/';
+      return home(method, res);
+  }
+}
+
 server.on('request', function(request, response) {
   var method = request.method;
   var url = request.url;
   var headers = request.headers;
-  console.log(method);
-  console.log(url);
-  console.log(request);
+  var body = [];
+
+  request.on('data', function(chunk) {
+    body.push(chunk);
+    console.log(chunk);
+  });
+
+  request.on('error', function(err) {
+    console.log(err.stack);
+  });
+
+  request.on('end', function() {
+    response.setHeader('Content-Type', 'applicatoin/json');
+
+    response.on('error', function(err) {
+      response.statusCode = 500;
+      res.end('Error: ', err)
+    });
+
+    var responseBody = {};
+    responseBody.headers = headers;
+    responseBody.method = method;
+    responseBody.url = url;
+    responseBody.body = body;
+    response.write(JSON.stringify(responseBody));
+    response.end()
+  });
+
+  router(method, url, response);
+  // console.log(method);
+  // console.log(url);
+  // console.log(request);
 });
 
 server.listen(port);
